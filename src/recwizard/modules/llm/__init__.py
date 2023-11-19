f9f2a47f6576878f554e1c2afd8b4ee41f1844aa
Zeyuan Zhang <kevin99zzy@gmail.com>
2023-11-18 21:34:45 -0800

    Fix import error

--- src/recwizard/modules/llm/__init__.py
+++ src/recwizard/modules/llm/__init__.py
@@ -1,7 +1,6 @@
 from .modeling_chatgpt_gen import ChatgptGen
 from .modeling_chatgpt_rec import ChatgptRec
 from .tokenizer_chatgpt import ChatgptTokenizer
-from .configuration_chatgpt_gen import ChatgptGenConfig
 from .configuration_llm_rec import LLMRecConfig
 from .configuration_llm import LLMConfig
 from .modeling_llama_gen import LlamaGen
